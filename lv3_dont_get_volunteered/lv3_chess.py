f123883bcbf81ac67507ccff24ff4a94d88af5d9
Aidan Brennan <89869586+Fuscior@users.noreply.github.com>
2023-05-26 20:40:20 +0100

    Update lv3_chess.py

--- lv3_dont_get_volunteered/lv3_chess.py
+++ lv3_dont_get_volunteered/lv3_chess.py
@@ -33,28 +33,54 @@ chess_board= [
     [56,57,58,59,60,61,62,63],
 ]
 
-print(chess_board[1][0])
-
 '''
 valid move finsih 
             locate x> 0 && x< 8
             locate y> 0 && y<8
 '''
 
-#work in prog
-start_locate=56
+src=0
+des=0
 
-x_locate= int(start_locate / 8)
-y_locate= start_locate % 8
+def Solution(src, des):
+    #work in prog
+    start_locate=src
+    destination=des
+    isFound=False
+    move_count=0
+    current_pos_list=[start_locate]
+    
+    if start_locate == destination:
+        return move_count
 
-print("The location is [", x_locate, "]", "[", y_locate, "]")
-print("The square number is: ",chess_board[x_locate][y_locate])
+    while isFound == False:
+        for j in range(len(current_pos_list)):
+            #print("the lenght of the array is ", len(current_pos_list))
+            x_locate= int(current_pos_list[j] / 8)
+            y_locate= current_pos_list[j] % 8
 
-##generate moves 8 possible
-for i in range(8):
-    x_move= x_locate + knight_moves[i][0]
-    y_move= y_locate + knight_moves[i][1]
-    if x_move >=0 and x_move <=7 and y_move >= 0 and y_move <=7:
-        #valid move
+            #print("The location is [", x_locate, "]", "[", y_locate, "]")
+            #print("The square number is: ",chess_board[x_locate][y_locate])
+            ##generate moves 8 possible
+            for i in range(8):
+                x_move= x_locate + knight_moves[i][0]
+                y_move= y_locate + knight_moves[i][1]
+                if x_move >=0 and x_move <=7 and y_move >= 0 and y_move <=7:
+                    current_pos_list.append(chess_board[x_move][y_move])
+            for x in range(len(current_pos_list)):
+                if current_pos_list[x] == destination:
+                    isFound=True
+        move_count= move_count +1
+
+
+        
+    #print(current_pos_list)
+    #print(move_count)
+    return move_count
+
+print(Solution(src, des))
+
+
+    
         
 
