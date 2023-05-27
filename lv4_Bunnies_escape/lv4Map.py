8927ee1e9ef924d5b8ba40c74bbee40bf4c0de1b
Aidan Brennan <89869586+Fuscior@users.noreply.github.com>
2023-05-27 22:13:35 +0100

    Update lv4Map.py

--- lv4_Bunnies_escape/lv4Map.py
+++ lv4_Bunnies_escape/lv4Map.py
@@ -9,24 +9,37 @@ example=[
 ]
 #answer 11
 
-map_list=example
+map_array=example
 
 map_rows= len(example)
 map_cols= len(example[0])
 print("The maps is",map_rows,"Rows", "by", map_cols,"cols")
 
-#stop top left check down if 1 move right 
-#keep how many moves per line 
-#re write the line with the most moves to 0s to delete
-#?? flaws
+#look in next row for 0 if zero save col index 
+#check vs current door 
+#min result is new door
+#biggest diff from prv door to new door = bigest wall to delete
 
-print(map_list)
 
 start_row=0
 start_col=0
 
-current_row=0
-current_col=0
+current_row=start_row
+current_col=start_col
 
-#for i in range(map_cols):
-    
+door_way=0
+possible_door_way=[]
+possible_move_length=[]
+total_moves=[]
+
+for i in range(map_cols):
+    if map_array[current_row+1][i] == 0:
+        possible_door_way.append(i)
+        print(possible_door_way)
+
+    for x in range(len(possible_door_way)):
+        possible_move_length.append(abs(door_way-possible_door_way[x]))
+        possible_move_length.sort()
+        print(possible_move_length)
+
+        total_moves.append(possible_move_length[0])
